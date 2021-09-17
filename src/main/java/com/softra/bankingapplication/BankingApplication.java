49a33f2f772b32979fa0aa638853912b7695bc39
ShiMinOng <ongshimin12@gmail.com>
2021-09-17 10:02:13 +0800

    include initial population of db tables

--- src/main/java/com/softra/bankingapplication/BankingApplication.java
+++ src/main/java/com/softra/bankingapplication/BankingApplication.java
@@ -1,13 +1,53 @@
 package com.softra.bankingapplication;
 
+import java.util.Arrays;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import com.softra.bankingapplication.models.CreateUsers;
 
 @SpringBootApplication
 public class BankingApplication {
+	
+	public static String consumePostRestApis(RestTemplate template, Map<String, Object> beforeMap) {
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<Map<String, Object>> entity = new HttpEntity<>(beforeMap,headers);
+		String url = "http://localhost:7777/users";
+		template.exchange(url, HttpMethod.POST, entity, String.class).getBody();
+		return null;
+	}
+	
+	@Bean(name="resttemplate")
+	public RestTemplate getRestTemplate() {
+		
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankingApplication.class, args);
+//		SpringApplication.run(BankingApplication.class, args);
+		
+		ApplicationContext context = SpringApplication.run(BankingApplication.class, args);
+		RestTemplate template = context.getBean("resttemplate", RestTemplate.class);
+		
+		CreateUsers cu = new CreateUsers();
+		Map<String, Object> map1 = cu.create1();
+		Map<String, Object> map2 = cu.create2();
+		Map<String, Object> map3 = cu.create3();
+		
+		//PLEASE COMMENT LINES 45 TO 47 AFTER THE FIRST RUN!!!!!!!!!!!!!
+		consumePostRestApis(template, map1);
+		consumePostRestApis(template, map2);
+		consumePostRestApis(template, map3);
 	}
 
 }
---
+++ src/main/java/com/softra/bankingapplication/models/CreateUsers.java
@@ -0,0 +1,79 @@
+package com.softra.bankingapplication.models;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
+
+@Component
+public class CreateUsers {
+	
+//		User adminUser = new User("Peh", "12345", "Some Secret Question", "12345", false, "ADMIN");
+//		User custUser1 = new User("John", "54321", "What is your favourite Animal", "54321", false, "CUSTOMER");
+//		User custUser2 = new User("James", "32121", "What is your favourite colour", "32121", false, "CUSTOMER");
+//		System.out.println("Inside 1");
+//		userdao.save(adminUser);
+//		System.out.println(adminUser);
+//		userdao.save(custUser1);
+//		userdao.save(custUser2);
+//		
+//		Customer c1 = new Customer(custUser1.getUserId(), "Johnny", "smth@gmail.com", "11 Springleaf Drive");
+//		Customer c2 = new Customer(custUser2.getUserId(), "James", "everything@gmail.com", "31 Park Villas Rise");
+//		System.out.println("Inside 2");
+//		custdao.save(c1);
+//		custdao.save(c2);
+//		
+//		Account acct1 = new Account("Savings", 500.00, new Date(), custUser1.getUserId());
+//		Account acct2 = new Account("Savings", 1000.00, new Date(), custUser2.getUserId());
+//		System.out.println("Inside 3");
+//		acctdao.save(acct1);
+//		acctdao.save(acct2);
+	
+	public Map<String, Object> create1() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("username", "Peh");
+		map.put("loginPassword", "12345");
+		map.put("secretQuestion", "Some Secret Question");
+		map.put("transactionPassword", "12345");
+		map.put("lockStatus", false);
+		map.put("role", "ADMIN");
+		return map;
+	}
+	
+	public Map<String, Object> create2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("username", "John");
+		map.put("loginPassword", "54321");
+		map.put("secretQuestion", "What is your favourite Animal");
+		map.put("transactionPassword", "54321");
+		map.put("lockStatus", false);
+		map.put("role", "CUSTOMER");
+		map.put("customerName", "Johnny");
+		map.put("email", "smth@gmail.com");
+		map.put("address", "11 Springleaf Drive");
+		map.put("accountType", "Savings");
+		map.put("accountBalance", 500.00);
+		return map;
+	}
+	
+	public Map<String, Object> create3() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("username", "James");
+		map.put("loginPassword", "32121");
+		map.put("secretQuestion", "What is your favourite Colour");
+		map.put("transactionPassword", "32121");
+		map.put("lockStatus", false);
+		map.put("role", "CUSTOMER");
+		map.put("customerName", "James");
+		map.put("email", "everything@gmail.com");
+		map.put("address", "31 Park Villas Rise");
+		map.put("accountType", "Savings");
+		map.put("accountBalance", 1000.00);
+		return map;
+	}
+}
